803b88a78d42c0a23306826fa54e4e2cf4a4a5d4
MoNezhadali <monezhadali@gmail.com>
2023-09-27 13:06:27 +0200

    Add director_name, movie_title, and sort_by to the API filters

--- APIapp3.py
+++ APIapp3.py
@@ -1,7 +1,7 @@
 # Python libraries
 import json, sqlite3
 # Site-packages
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 # Define the application as an instance of Flask
 app=Flask(__name__)
@@ -9,30 +9,53 @@ app=Flask(__name__)
 # Define the sqlite database file
 DATABASE = 'movie.sqlite'
 
+
+
 @app.route('/api/movies', methods=['GET'])
-def get_all_movies():
-    # Define the appropriate query
-    query =  '''SELECT original_title, director_id
-                FROM movies
-                LIMIT 10;'''
-    # query = 'SELECT original_title FROM movies'
+def get_movies():
+    print(1111111111111111111111)
+    # Get query parameters for filtering and sorting
+    director_name = request.args.get('director_name')
+    film_title = request.args.get('movie_title')
+    sort_by = request.args.get('sort_by')
+
+    print(director_name)
+    print(film_title)
     
+    # Define the base query
+    query = '''SELECT m.*, d.name AS director_name
+               FROM movies m
+               JOIN directors d ON m.director_id = d.id'''
+
+    # Apply filters
+    if director_name:
+        query += f" WHERE d.name LIKE '%{director_name}%'"
+    if film_title:
+        if director_name:
+            query += " AND"
+        else:
+            query += " WHERE"
+        query += f" m.original_title LIKE '%{film_title}%'"
+
+    # Apply sorting
+    if sort_by:
+        query += f" ORDER BY {sort_by}"
+
+    # Limit the number of results to 10
+    query += ' LIMIT 10;'
+
     # Send the query to the execute_query function
     result = execute_query(query)
-    # Jsonify the results as demanded
     return jsonify(result)
 
 @app.route('/api/directors', methods=['GET'])
-def get_all_directors():
+def get_directors():
     # Define the appropriate query
-    query='SELECT name FROM directors'
+    query = 'SELECT name FROM directors'
     # Send the query to the execute_query function
-    result=execute_query(query)
-    # Jsonify the results
-    json_data = json.dumps(result, indent=4)
-
-    return json_data
-    # return jsonify(result)
+    result = execute_query(query)
+    # json_data = json.dumps(result, indent=4)
+    return jsonify(result)
 
 def execute_query(query):
     # Connect to the database
@@ -51,12 +74,3 @@ def execute_query(query):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-    
-
-
-
-
-
