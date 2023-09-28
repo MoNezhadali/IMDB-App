841305c88feb9b16a7f8bf4df5c799095d11f4c7
MoNezhadali <monezhadali@gmail.com>
2023-09-28 16:14:33 +0200

    Merge the API and web interface apps

--- App.py
+++ App.py
@@ -1,39 +1,149 @@
 # Python libraries
-import sqlite3, time, json
+import os, sqlite3
 # Site-packages
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, jsonify, request
 
-
+# Make the app
 app = Flask(__name__)
 
-# Define the SQLite database file
-DATABASE = 'movie.sqlite'
+# Define the Exceptions
+class UnknownArgumentException(Exception):
+    pass
+
+class NoSuchColumnException(Exception):
+    pass
+
+
+
+# Define the sqlite database file
+DATABASE = os.path.abspath(os.path.join(os.path.dirname(__file__),'movie.sqlite'))
+
+
+def get_db_connection():
+    connection = sqlite3.connect(DATABASE)
+    return connection, connection.cursor()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/execute_sql', methods=['POST'])
 def execute_sql():
     query = request.form['query']
-    result = execute_query(query)
+    result = execute_query(query, output_format='interface')
     return jsonify(result)
 
-def execute_query(query):
-    connection = sqlite3.connect(DATABASE)
-    cursor = connection.cursor()
-    cursor.execute(query)
-    if query.lower().startswith('select'):
-        result = cursor.fetchall()
-        column_names = [desc[0] for desc in cursor.description]
-        cursor.close() # best practice
-        connection.close()
-        return {'data': result, 'columns': column_names}
-    else:
-        connection.commit()
+
+@app.route('/api/movies', methods=['GET'])
+def get_movies():
+    allowed_args = ['director_name', 'movie_title', 'sort_by']
+
+    # Check for any unknown arguments
+    for arg in request.args.keys():
+        if arg not in allowed_args:
+            raise UnknownArgumentException(f"Unknown argument '{arg}' provided.")
+
+
+    # Get query parameters for filtering and sorting
+    director_name = request.args.get('director_name')
+    movie_title = request.args.get('movie_title')
+    sort_by = request.args.get('sort_by')
+    
+    # Define the base query
+    query = '''SELECT m.title
+               FROM movies m
+               JOIN directors d ON m.director_id = d.id'''
+
+    # Apply filters
+    if director_name:
+        query += f" WHERE d.name LIKE '%{director_name}%'"
+    if movie_title:
+        if director_name:
+            query += " AND"
+        else:
+            query += " WHERE"
+        query += f" m.original_title LIKE '%{movie_title}%'"
+
+    # Apply sorting
+    if sort_by:
+        if sort_by=='movie_title':
+            sort_by='title'
+        if sort_by=='director_name':
+            sort_by='d.name'
+        query += f" ORDER BY {sort_by}"
+    query += ';'
+
+    # Send the query to the execute_query function
+    result = execute_query(query, output_format='api')
+    
+    # Send the API call response
+    return jsonify({"status": 200, "result": result})
+
+
+
+@app.route('/api/directors', methods=['GET'])
+def get_directors():
+    # Get query parameters for  sorting
+    sort_by = request.args.get('sort_by')
+    director_name = request.args.get('director_name')
+    # Define the appropriate query
+    query = 'SELECT name FROM directors'
+    if director_name:
+        query += f" WHERE name LIKE '%{director_name}%'"
+    if sort_by:
+        if sort_by=='director_name':
+            sort_by='name'
+        query += f" ORDER BY {sort_by}"
+    query+=';'
+    
+    # Send the query to the execute_query function
+    result = execute_query(query, output_format='api')
+
+    # Send the API call response
+    return jsonify({"status": 200, "result": result})
+
+
+def execute_query(query, output_format='api'):
+    connection, cursor = get_db_connection()
+
+    try:
+        cursor.execute(query)
+        
+        if query.lower().startswith('select'):
+            columns = [desc[0] for desc in cursor.description]
+            result = cursor.fetchall()
+            
+            if output_format == 'api':
+                return [dict(zip(columns, row)) for row in result]
+            elif output_format == 'interface':
+                return {'data': result, 'columns': columns}
+            else:
+                raise ValueError(f"Unsupported output_format: {output_format}")
+        else:
+            connection.commit()
+            return {'message': 'Query executed successfully.'}
+    except sqlite3.OperationalError as e:
+        if "no such column" in str(e):
+            raise NoSuchColumnException("The specified column doesn't exist.")
+        raise Exception("Database error occurred.")
+    finally:
         cursor.close()
         connection.close()
-        return {'message': 'Query executed successfully.'}
+
+
+@app.errorhandler(UnknownArgumentException)
+def handle_unknown_arg_error(e):
+    return jsonify({"status": 400, "error": str(e)}), 400
+
+@app.errorhandler(NoSuchColumnException)
+def handle_no_such_column_error(e):
+    return jsonify({"status": 400, "error": str(e)}), 400
+
+@app.errorhandler(Exception)
+def handle_generic_error(e):
+    return jsonify({"status": 500, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True)
